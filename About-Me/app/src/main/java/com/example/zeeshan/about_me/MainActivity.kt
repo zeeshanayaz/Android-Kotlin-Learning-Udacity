1d7f84f2ea0100efbe3ec10a97c7eab19b5fc22d
Zeeshan Ayaz <zeeshanayaz1@gmail.com>
2018-11-17 18:38:02 +0500

    Created About Me App - Project 2 - Udacity Course

---
+++ About-Me/app/src/main/java/com/example/zeeshan/about_me/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.zeeshan.about_me
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
